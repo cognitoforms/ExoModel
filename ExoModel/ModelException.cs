9420f199924cfac09d3ed4d748dab6484c25bdad
Peter <peter.gindhart@vc3.com>
2013-04-12 10:08:59 -0400

    Add ModelException for customizing client error messages.

---
+++ ExoModel/ModelException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExoModel
+{
+	/// <summary>
+	/// Wrap exceptions in this type to control the information displayed in the UI
+	/// when a more abstracted or informative message is desired.
+	/// 
+	/// See ServiceHandler.ProcessRequest for implications
+	/// </summary>
+	public class ModelException : ApplicationException
+	{
+		public ModelException(string message, Exception innerException)
+			: base(message, innerException)
+		{}
+	}
+}
